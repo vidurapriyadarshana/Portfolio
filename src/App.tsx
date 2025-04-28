d4dadcc77f8136d5a235497554c4c14eaa991a28
Vidura Priyadarshana <vidura200427@gmail.com>
2025-04-28 18:16:49 +0530

    2025-04-28 | 06:16

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './components/Header'
+import SideBar from './components/SideBar'
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <SideBar />
+    </>
+  )
+}
+
+export default App
